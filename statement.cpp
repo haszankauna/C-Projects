1616403155c06ec8faeb6ef944f61e01c3d19c79
haszanakauna <haszankauna@gmail.com>
2023-10-19 06:37:12 +0100

    added the file containing the statements in C++

---
+++ statement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+    //if statements = do something if a conditio is true
+    //if not, then don't do it.
+    int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if(age >= 18){
+        std::cout << "Welcome to the site!";
+    }
+    else if (age < 0){
+        std::cout << "You haven't been born yet!";
+    }
+    else{
+        std::cout << "You are not old enough to enter!";
+    }
+
+
+    return 0;
+}
