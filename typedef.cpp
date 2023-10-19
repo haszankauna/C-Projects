f25f52a65460ada5d5a6276c0331244c1ac9327b
haszanakauna <haszankauna@gmail.com>
2023-10-19 05:02:20 +0100

    commit

---
+++ typedef.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    // typedef = reserved keyword used to create an additional name
+    // (alias) for another data type.
+    // New identifier for an existing type
+    // Helps with readability and reduces types
+    
+
+
+    
+    return 0;
+}
