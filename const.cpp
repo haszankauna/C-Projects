256591152be00f4fe273e328efb9a13224d66fe8
haszanakauna <haszankauna@gmail.com>
2023-10-19 04:48:30 +0100

    adding the const file to showcase the use of const in C++

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    //to showcase the use of const in C++
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    
+
+
+    return 0;
+}
