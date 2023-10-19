adac67351bdef70ae7db7e7f586c3fc46403659d
haszanakauna <haszankauna@gmail.com>
2023-10-19 04:34:53 +0100

    first commit

---
+++ age.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+    //integer (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    //double(number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.2;
+
+
+    //single character
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    //boolean(true or false)    
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+
+    // string(objects that represents a sequence of text)
+
+
+    std::string name = "Kauna";
+
+    std::cout << name;
+
+
+
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+
+    int x = 5; //declaration
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    return 0;
+}
