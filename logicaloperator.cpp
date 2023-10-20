6e3d1d8e2016f562b9b546370e9f41f349e1732e
haszanakauna <haszankauna@gmail.com>
2023-10-20 11:42:52 +0100

    updated logical operator file

--- logicaloperator.cpp
+++ logicaloperator.cpp
@@ -6,15 +6,23 @@ int main(){
     // ! = reverse the logical state of its operand
 
     int temp;
+    bool sunny = false;
 
     std::cout << "Enter the temperature: ";
     std::cin >> temp;
 
 
-    if(temp > 0 && temp < 30){
-        std::cout << "The temperature is good!";
+    if(temp <= 0 || temp >= 30){
+        std::cout << "The temperature is bad!\n";
     }
     else{
-        std::cout << "The temperature is bad";
+        std::cout << "The temperature is good!\n";
+    }
+
+    if(sunny){
+        std::cout << "it is sunny outside!";
+    }
+    else{
+        std::cout << "it is cloudy outside";
     }
 }
